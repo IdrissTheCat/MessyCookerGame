d44e78bfaa326cb91748aeb9fffe93d958b45e9c
IdrissTheCat <69967311+IdrissTheCat@users.noreply.github.com>
2020-11-03 23:55:45 +0100

    Add files via upload
    second day

--- project/Game.js
+++ project/Game.js
@@ -2,79 +2,96 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const backgroundImg = new Image();
-backgroundImg.src="Img/backgroundKitchen.jpg";
+backgroundImg.src = "Img/backgroundKitchen.jpg";
 
-class Game{
+const basketImg = new Image();
+basketImg.src = "Img/basket-png.png";
 
-  constructor(background,player){
-    this.background=background;
+class Game {
+  constructor(background, player) {
+    this.background = background;
+    this.player = player;
     this.animationId;
   }
 
-  updateGame=()=>{
-
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+  updateGame = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     this.background.move();
     this.background.draw();
-     
-    this.animationId=requestAnimationFrame(this.updateGame)
+
+    this.player.move();
+    this.player.draw();
+
+    this.animationId = requestAnimationFrame(this.updateGame);
+  };
+}
+
+class Background {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = 0;
+  }
+
+  // Queria que tivesse a opçao de  aumentar ou diminuir o speed do background img.
+  // para ir para frente ou trás, mesmo que nesse caso eu va usar o speed zero nesse momento.
+  //entao criei else if  na funcao caso eu queira usar outros fundos mutaveis.
+
+  move() {
+    this.y += this.speed;
+    //    if(this.y <0 ) {
+    //    this.y=canvas.height;
+    //   }
+
+    this.y %= canvas.height;
+  }
+
+  draw() {
+    ctx.drawImage(backgroundImg, this.x, this.y, this.width, this.height);
+    if (this.speed >= 0) {
+      ctx.drawImage(
+        backgroundImg,
+        this.x,
+        this.y - canvas.height,
+        this.width,
+        this.height
+      );
+    }
+    if (this.speed < 0) {
+      ctx.drawImage(
+        backgroundImg,
+        this.x,
+        this.y + canvas.height,
+        this.width,
+        this.height
+      );
+    }
   }
 }
 
+class Player {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = 0;
+  }
+  move() {
+    this.x += this.speed;
 
-
-
-class Background{
-  constructor(x, y, width,height) {
-  this.x=x;
-  this.y=y;
-  this.width=width;
-  this.height=height;
-  this.speed =0;
-
-
-}
-
-
-// Queria que tivesse a opçao de  aumentar ou diminuir o speed do background img.
-// para ir para frente ou trás, mesmo que nesse caso eu va usar o speed zero nesse momento.
-//entao criei else if  na funcao caso eu queira usar outros fundos mutaveis.
-
- move(){
-   this.y += this.speed;
-   if(this.y >= canvas.height) {
-     this.y=0;
-   }
+    if (this.x <= 0) {
+      this.x = 0;
+    }
+    if (this.x >= canvas.width) this.x = canvas.width;
   }
 
- draw(){
- ctx.drawImage(backgroundImg, this.x, this.y, this.width, this.height);
- if(this.speed>=0){
-   ctx.drawImage(
-     backgroundImg,
-     this.x,
-     this.y-canvas.height,
-     this.width,
-     this.height
-
-   );
-
- }
-  else if(this.speed<0){
-   ctx.drawImage(
-     backgroundImg,
-     this.x,
-     this.y +canvas.height,
-     this.width,
-     this.height
-
-   );
-
- }
-
- }
-
+  draw() {
+    ctx.drawImage(basketImg, this.x, this.y, this.width, this.height);
+  }
 }
 
 window.onload = () => {
@@ -82,11 +99,12 @@ window.onload = () => {
     startGame();
   };
 
-
   function startGame() {
-    const game=new Game(new Background(0,0,canvas.width,canvas.height));
+    const game = new Game(
+      new Background(0, 0, canvas.width, canvas.height),
+      new Player(canvas.width / 2, canvas.height - 200, 130, 130)
+    );
 
     game.updateGame();
-
   }
 };
